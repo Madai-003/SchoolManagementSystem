14e42856c310b64d05d90658702d2d87b4170708
Madai-003 <perezmadai9702003@gmail.com>
2025-07-25 18:21:42 -0600

    anadiendo clase ServiciosAcademicosI

---
+++ src/main/java/ejercicio/ServiciosAcademicosI.java
@@ -0,0 +1,10 @@
+package ejercicio;
+
+public interface ServiciosAcademicosI
+{
+
+    void matricularEstudiante(Estudiante estudiante);
+    void agregarCurso(Curso curso);
+    void inscribirEstudianteCurso(Estudiante estudiante, int idCurso) throws EstudianteYaInscritoException;
+    void desinscribirEstudianteCurso(int idEstudiante, int idCurso) throws EstudianteNoInscritoEnCursoException;
+}
