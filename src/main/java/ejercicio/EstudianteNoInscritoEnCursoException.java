4771bbf0c8376666bf3d0f685a57b75f81616e57
Madai-003 <perezmadai9702003@gmail.com>
2025-07-25 18:23:45 -0600

    anadiendo clase EstudianteNoInscritoEnCursoException

---
+++ src/main/java/ejercicio/EstudianteNoInscritoEnCursoException.java
@@ -0,0 +1,7 @@
+package ejercicio;
+
+public class EstudianteNoInscritoEnCursoException extends Exception {
+    public EstudianteNoInscritoEnCursoException(String mensaje) {
+        super(mensaje);
+    }
+}
